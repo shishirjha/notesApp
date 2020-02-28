c02c9057cdd3ab583bd57676aa5706c163576263
Shishir Jha <shishirjhaccr@gmail.com>
2020-02-28 23:20:29 +0530

    simple calling of require methd without assigning it

--- app.js
+++ app.js
@@ -15,5 +15,11 @@
 
 // -------------------------- Importing Created Modules --------------------------- //
 
+// When we use require to import our own modules, we need to provide relative path.
 
+// when we call require method directly as shown below, it directly executes the concerned file.
+require("./utils.js");
+const name = "Shishir";
+
+console.log(name);
 
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
 // demo of how to use and import your own modules in nodejs
 
 // This file utils.js represents the file which will contain some utilities for out app
+
+console.log("utils.js");
